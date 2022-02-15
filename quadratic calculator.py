3486c65f2143f9c4f31e78fe719b873126268e5c
lunaraj <98667299+lunaraj@users.noreply.github.com>
2022-02-15 16:01:24 -0500

    Update quadratic calculator.py

--- quadratic calculator.py
+++ quadratic calculator.py
@@ -1,4 +1,5 @@
 import random
+import time
 aValue = int(input('what is your a value '))
 bValue = int(input('what is your b value '))
 cValue = int(input('what is your c value '))
@@ -6,15 +7,21 @@ multValue = aValue * cValue
 addValue = bValue
 factor1 = 0
 factor2 = 0
+guesses = 0
 while True:
     if factor1 + factor2 == addValue and factor1 * factor2 == multValue:
         break
     elif multValue < 0:
         factor1 = random.randint(multValue-1, -multValue+1)
         factor2 = random.randint(multValue-1, -multValue+1)
+        guesses+= 1
     elif multValue > 0:
         factor1 = random.randint(-multValue-1, multValue+1)
         factor2 = random.randint(-multValue-1, multValue+1)
+        guesses += 1
+    if guesses > 1000000:
+        print('there are no integer values')
+        time.sleep(100)
 divisor = aValue
 while divisor > 0:
     if aValue%divisor == 0 and factor1%divisor == 0:
@@ -38,7 +45,7 @@ if factor2 < 0:
 else:
     neg2 = '+'
 print('(' + str(int(aValue/factor1Gcf)) + 'x' + neg + str(int(factor1/factor1Gcf)) + ')' + '(' + str(int(aValue/factor2Gcf)) + 'x' + neg2 + str(int(factor2/factor2Gcf)) + ')')
-
+print('number of guesses: ' + str(guesses))
 
 
     
