0f92d0b3e01ae49fa86a91aa66a81f2ba7154aaa
lunaraj <98667299+lunaraj@users.noreply.github.com>
2022-02-14 22:13:41 -0500

    Update lowest monthly set payment.py

--- lowest monthly set payment.py
+++ lowest monthly set payment.py
@@ -5,9 +5,9 @@ bal = balance
 count = 12
 mplb = balance/12
 mpub = (balance*(1+mir)**12)/12
-lmp = (mplb+mpub)/2
 epsilon = 0.01
 while True:
+    lmp = (mplb+mpub)/2
     bal = balance
     count = 12
     while count > 0:
@@ -17,7 +17,6 @@ while True:
     if abs(bal) <= epsilon:
         break
     else:
-        print(lmp)       
         if bal < 0:
             mpub = lmp
         elif bal > 0:
